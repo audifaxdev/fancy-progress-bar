f056d301bd5609a7a1c1f31a9e76177baa4f89e2
SneakyC4 <sneakyc4@Hackintosh.local>
2015-09-21 18:37:39 +1000

    move ts files out of application folder

---
+++ ts/components/skillrating/skillrating.ts
@@ -0,0 +1,26 @@
+/// <reference path="../../import.ts" />
+/// <reference path="../common.ts" />
+/// <reference path="../progressbar/progressbar.ts" />
+
+@ng2.Component({
+	selector: 'skill-rating',
+	viewBindings: [ng2Http.HTTP_BINDINGS],
+	properties: ['flexitemwidth']
+})
+
+@ng2.View({
+	templateUrl: `${COMPONENTS_ROOT_PATH}/skillrating/skillrating.tpl.html`,
+	styleUrls: [`${COMPONENTS_ROOT_PATH}/skillrating/skillrating.css`],
+	directives: [ng2.NgFor, ProgressBar]
+})
+
+class SkillRating {
+	skills: Object;
+	constructor(@ng2.Inject(ng2Http.Http) http: typeof ng2Http.Http) {
+		console.log(this);
+		http.get('data/skills.json')
+		.toRx()
+		.map(res => res.json())
+		.subscribe(skills => this.skills = skills);
+	}
+}
---
+++ ts/import.ts
@@ -0,0 +1,2 @@
+var ng2 = require( 'angular2/angular2' );
+var ng2Http = require('angular2/http');
